e8dc6fefa5d307a6d1dcacd29745c180d10f0a2b
NiviDevs <nevedya.gwalani@gmail.com>
2025-06-17 14:48:33 +0530

    hi test commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,7 @@ function App() {
     return (
         <div className="items-center place-items-center align-middle blur-2xl">
             <h1 className="text-center bg-blue-300 p-1">Hello React</h1>
-            <Button
-                className="bg-center align-top flex items-center"
-                variant="default"
-            >
-                negga
-            </Button>
+
         </div>
     );
 }
