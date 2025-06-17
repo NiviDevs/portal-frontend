ad98af03a6302775573535d9f6f93460003833cc
panshul-07 <panshulajpechetty@gmail.com>
2025-06-17 15:21:45 +0530

    changed backround

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import "./App.css";
 import { Button } from "./components/ui/button";
 function App() {
     return (
-        <div className="items-center place-items-center align-middle blur-2xl">
+        <div className="items-center place-items-center align-middle ">
             <h1 className="text-center bg-blue-300 p-1">Hello React</h1>
-
         </div>
     );
 }
