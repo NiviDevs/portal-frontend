7b630aa88a8d8f5bd3cd33016eb6426555e85337
NiviDevs <nevedya.gwalani@gmail.com>
2025-07-15 22:55:00 +0530

    separated login and register

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import { ThemeProvider } from "./components/theme-provider";
 import AboutPage from "./pages/AboutPage";
 import DashboardPage from "./pages/DashboardPage";
 import LandingPage from "./pages/LandingPage";
-import NewAuthPage from "./pages/NewAuthPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+// import NewAuthPage from "./pages/NewAuthPage";
+import TestPage from "./pages/TestPage";
 
 function App() {
 	return (
@@ -14,10 +17,12 @@ function App() {
 			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
 				<TopNav />
 				<Routes>
-					<Route path="" element={<LandingPage />}></Route>
-					<Route path="/auth" element={<NewAuthPage />}></Route>
+					<Route path="/" element={<LandingPage />}></Route>
+					<Route path="/login" element={<LoginPage />}></Route>
+					<Route path="/register" element={<RegisterPage />}></Route>
 					<Route path="/about" element={<AboutPage />}></Route>
 					<Route path="/dashboard" element={<DashboardPage />}></Route>
+					<Route path="/test" element={<TestPage />}></Route>
 				</Routes>
 				{/* <BottomNav/> */}
 				<Toaster position="top-left" />
